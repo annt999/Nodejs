f25bc5c5bdb2b254690fd461da23dfce0fcbc0e4
An Nguyễn <annt9934@gmail.com>
2022-02-07 07:09:30 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 
+const route = require('./routes');
+const db = require('./config/db');
+const bp = require('body-parser');
+
+db.connect();
+
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 app.engine(
     'hbs',
@@ -22,14 +31,8 @@ app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('new');
-});
+route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
